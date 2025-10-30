99559460d5abcfbd386bb685b7f9e6d91f34ccbd
Vihaan Kanwar <vihaankanwar35@gmail.com>
2025-10-30 18:08:18 +0530

    Update api.py

--- api.py
+++ api.py
@@ -66,6 +66,12 @@ def handle_message(data):
 def handle_disconnect():
     pass
 
+@app.after_request
+def add_cors_headers(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "*"
+    return response
+
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
-
